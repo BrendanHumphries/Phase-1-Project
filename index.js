2f4c9667ca37e7e3f25bd1974a1ec8201b5f595c
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-10-05 11:32:47 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Fetch and automatically render first band in display section of page
     fetch('https://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay') // Replace 'coldplay' with a featured band name
     .then(resp => resp.json())
-    .then(json => renderFirstBandOnly(json))
+    .then(json => renderBand(json))
     .catch(error => console.error(`Error: ${error}`));
 
-
+    // Fetch data for each of the three featured artists and then render them when clicked
+    fetch('https://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay') // Replace 'coldplay' with a featured band name
+    .then(resp => resp.json())
+    .then(json => {
+        const artistNameItem = document.querySelector('#artist-list').children[0];
+        artistNameItem.addEventListener('click', () => {
+            console.log(`I've been clicked! (#1)`);
+            renderBand(json);
+        })
+    })
+    .catch(error => console.error(`Error: ${error}`));
+    fetch('https://www.theaudiodb.com/api/v1/json/1/search.php?s=odesza') // Replace 'odesza' with a featured band name
+    .then(resp => resp.json())
+    .then(json => {
+        const artistNameItem = document.querySelector('#artist-list').children[1];
+        artistNameItem.addEventListener('click', () => {
+            console.log(`I've been clicked! (#2)`);
+            renderBand(json);
+        })
+    })
+    .catch(error => console.error(`Error: ${error}`));
+    fetch('https://www.theaudiodb.com/api/v1/json/1/search.php?s=daft_punk') // Replace 'daft_punk' with a featured band name
+    .then(resp => resp.json())
+    .then(json => {
+        const artistNameItem = document.querySelector('#artist-list').children[2];
+        artistNameItem.addEventListener('click', () => {
+            console.log(`I've been clicked! (#3)`);
+            renderBand(json);
+        })
+    })
+    .catch(error => console.error(`Error: ${error}`));
 })
 
-function renderFirstBandOnly(json) {
+function renderBand(json) {
     const bandObject = json.artists[0];
     
     // Upper pannel information
@@ -29,18 +60,9 @@ function renderFirstBandOnly(json) {
     // Lower pannel information
     const bandDescription = bandObject.strBiographyEN;
 
-    // console.log(bandBanner);
-    // console.log(bandName);
-    // console.log(bandYear);
-    // console.log(bandStyleAndGenre);
-    // console.log(bandMembers);
-    // console.log(bandOrigin);
-    // console.log(bandThumbnail);
-    // console.log(bandWebsite);
-    // console.log(bandFacebook);
-    // console.log(bandTwitter);
-    // console.log(bandDescription);
-
-    
+    // Select data containers
+    const upperPannelContainer = document.querySelector('#ID-TBD') // Need div id
+    const rightPannelContainer = document.querySelector('#info-div');
+    const leftPannelContainer = document.querySelector('#image-div');
+    const lowerPannelContainer = document.querySelector('#ID-TBD'); // Need div id
 }
-
